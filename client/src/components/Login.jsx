716082afbb0b6a415663b425600a1f9f0cd75961
chataw <tawcharlene@gmail.com>
2017-07-12 23:12:22 +0800

    React Login UI

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import "../stylesheets/login.scss"
 
 class Login extends Component {
 
-  componentDidMount() {
-    console.log("login");
-  }
-
   render() {
-    return null
+    return (
+      <section className="login-form">
+        <div className="container"><div className="clearbig" />
+          <div className="row">
+            <div className="text-center"> Enrollment System </div><br/>
+
+          <div className="row">
+
+            <form className="form-signin">
+              <img src="/public/images/sm-logo.png" className="img-responsive divider"/>
+
+              <div className="field clearsmall">
+                <input type="email" className="form-control input-sm" placeholder="Email"/>
+              </div>
+
+              <div className="field">
+                <input type="password" className="form-control" placeholder="Password" />
+              </div>
+
+              <div className="field">
+                <div className="checkbox">
+                  <label>
+                    <input type="checkbox" /> Remember Me
+                  </label>
+                </div>
+              </div>
+
+              <div className="actions clearsmall">
+                <button type="submit" className="btn btn-lg btn-primary btn-block">Log in</button>
+              </div>
+            </form>
+
+          </div>
+
+          <div className="row"><br/>
+            <div className="text-center small-text">© Caleb 2017 | All rights reserved</div>
+          </div>
+
+          </div>
+        </div>
+      </section>
+    );
   }
 
 }
